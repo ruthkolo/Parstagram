c1166145f339b248b885368507657f1c2ee1fa83
RuthKolo <koloruthy10@gmail.com>
2021-10-14 01:05:22 -0400

    Initial Commit

---
+++ parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagram
+//
+//  Created by Ruth kolo on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
